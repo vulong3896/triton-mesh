bfd9441b2f01391db8dc6844d4ebebda6144434d
Vu Long <Vu Long>
2025-09-09 10:07:08 +0700

    Add postgres database configuration

--- src/mesh/settings.py
+++ src/mesh/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = "mesh.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
     }
 }
 
